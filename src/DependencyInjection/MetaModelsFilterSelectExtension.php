66971fd51d7029ffbb5878c66276c9d05b5e07f3
Ingolf Steinhardt <git@e-spin.de>
2024-01-02 16:38:48 +0100

    Fixes for PHPCQ2

--- src/DependencyInjection/MetaModelsFilterSelectExtension.php
+++ src/DependencyInjection/MetaModelsFilterSelectExtension.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_select.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2024 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,7 +12,8 @@
  *
  * @package    MetaModels/filter_select
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2024 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_select/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -32,7 +33,7 @@ class MetaModelsFilterSelectExtension extends Extension
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
--- src/Resources/contao/dca/tl_metamodel_filtersetting.php
+++ src/Resources/contao/dca/tl_metamodel_filtersetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_select.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2023 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,10 +17,17 @@
  * @author     David Molineus <mail@netzmacht.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2021 The MetaModels team.
+ * @copyright  2012-2023 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_select/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
+
+$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+config'][] =
+    'select_where';
+$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+config'][] =
+    'select_filter';
+$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+config'][] =
+    'select_filterparams';
 
 $GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+fefilter'][] =
     'urlparam';
@@ -46,3 +53,39 @@ $GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends
     'skipfilteroptions';
 $GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+fefilter'][] =
     'cssID';
+
+$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['fields']['select_where'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['select_where'],
+    'exclude'   => true,
+    'inputType' => 'textarea',
+    'sql'       => 'text NULL',
+    'eval'      => [
+        'tl_class'       => 'clr',
+        'style'          => 'height: 4em;',
+        'decodeEntities' => 'true'
+    ]
+];
+
+$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['fields']['select_filter'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['select_filter'],
+    'exclude'   => true,
+    'inputType' => 'select',
+    'sql'       => 'int(11) unsigned NOT NULL default \'0\'',
+    'eval'      => [
+        'includeBlankOption' => true,
+        'alwaysSave'         => true,
+        'submitOnChange'     => true,
+        'tl_class'           => 'clr w50',
+        'chosen'             => 'true'
+    ],
+];
+
+$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['fields']['select_filterparams'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['select_filterparams'],
+    'exclude'   => true,
+    'inputType' => 'mm_subdca',
+    'sql'       => 'text NULL',
+    'eval'      => [
+        'tl_class' => 'clr m12'
+    ]
+];
