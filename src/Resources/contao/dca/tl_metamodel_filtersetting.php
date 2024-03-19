d007cc926b690679e8b28b6d4c64f7d15a6147d5
Ingolf Steinardt <info@e-spin.de>
2024-03-19 17:38:03 +0100

    Fixes DCA from testcase lines

--- src/Resources/contao/dca/tl_metamodel_filtersetting.php
+++ src/Resources/contao/dca/tl_metamodel_filtersetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_select.
  *
- * (c) 2012-2023 The MetaModels team.
+ * (c) 2012-2024 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,17 +17,10 @@
  * @author     David Molineus <mail@netzmacht.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2023 The MetaModels team.
+ * @copyright  2012-2024 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_select/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
-
-$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+config'][] =
-    'select_where';
-$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+config'][] =
-    'select_filter';
-$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+config'][] =
-    'select_filterparams';
 
 $GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['metapalettes']['select extends _attribute_']['+fefilter'][] =
     'urlparam';
